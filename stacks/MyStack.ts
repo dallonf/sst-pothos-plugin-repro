46bcd421e2d17d0f11e21b6cc579de0e2f8994fa
Dallon Feldner <dallon@sibipro.com>
2024-05-23 12:32:27 -0500

    Remove extra stuff

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,22 +1,8 @@
 import { StackContext, Api, EventBus } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const bus = new EventBus(stack, "bus", {
-    defaults: {
-      retries: 10,
-    },
-  });
-
   const api = new Api(stack, "api", {
-    defaults: {
-      function: {
-        bind: [bus],
-      },
-    },
     routes: {
-      "GET /": "packages/functions/src/lambda.handler",
-      "GET /todo": "packages/functions/src/todo.list",
-      "POST /todo": "packages/functions/src/todo.create",
       "POST /graphql": {
         type: "graphql",
         function: "packages/functions/src/graphql.handler",
@@ -26,10 +12,6 @@ export function API({ stack }: StackContext) {
         },
       },
     },
-  });
-
-  bus.subscribe("todo.created", {
-    handler: "packages/functions/src/events/todo-created.handler",
   });
 
   stack.addOutputs({
